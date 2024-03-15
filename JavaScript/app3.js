53d4b14f2986a474c7f8565cbe3e5b88a3c4e811
Deepak Yadav <deepakyadav11767@gmail.com>
2024-03-15 15:00:26 +0530

    JS3

--- JavaScript/app3.js
+++ JavaScript/app3.js
@@ -1,4 +1,4 @@
-alert("Working");
+ alert("Working");
 
 //Trim()
 let msg = "   Hello   ";
@@ -36,3 +36,50 @@ let students =["viper" , "suraj" ,"deepak"];
 let fruits =["MANGO" , "APPLE" , "LITCHI"];
 fruits[0] = "banana";
 console.log(fruits);
+
+// ARRAY METHODS //
+let cars =["audi","bmw","xuv","maruti"];
+console.log(cars); //['audi', 'bmw', 'xuv', 'maruti']
+cars.push("toyota");
+cars.push("ferrari");
+console.log(cars);//['audi', 'bmw', 'xuv', 'maruti', 'toyota', 'ferrari']
+cars.pop();
+console.log(cars);// ['audi', 'bmw', 'xuv', 'maruti', 'toyota']
+cars.unshift("ferrari");
+console.log(cars);//['ferrari', 'audi', 'bmw', 'xuv', 'maruti', 'toyota']
+cars.shift();
+console.log(cars);//['audi', 'bmw', 'xuv', 'maruti', 'toyota']
+
+let primary =['red','yellow','blue'];
+console.log(primary.indexOf('yellow'));
+console.log(primary.indexOf('green'));
+console.log(primary.includes('yellow'));
+console.log(primary.includes('green'));
+let secondary =["orange","green","violet"];
+console.log(primary.concat(secondary));
+console.log(primary.reverse());
+
+let color = ["red","yellow","blue","orange","pink","white"];
+console.log(color.slice());
+console.log(color.slice(2));
+console.log(color.slice(2,3));
+console.log(color.slice(-2));
+
+console.log(color.splice(4));
+console.log(color);
+console.log(color.splice(0,1));
+console.log(color);
+console.log(color.splice(0,1,"black","grey"));
+console.log(color);
+
+// ARRAY REFREENCES //
+let arr1 = ['a','b'];
+let arrCopy = arr1;
+arrCopy.push('c');
+console.log(arr1); //['a','b','c']
+console.log(arr1==arrCopy); //true
+
+// Nested Arrays //
+let nums =[[2,4],[3,6],[4,8]];
+console.log(nums); //(3) [Array(2), Array(2), Array(2)]
+console.log(nums.length);//3
