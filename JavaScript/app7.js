ae2f35b39266385b8def4934ec17a9bb0b94033c
Deepak Yadav <deepakyadav11767@gmail.com>
2024-03-24 15:43:59 +0530

    JS7

--- JavaScript/app7.js
+++ JavaScript/app7.js
@@ -1,18 +1,18 @@
 alert("Working");
 
-const student = {
-    name: "Suraj",
-    age: 20,
-    eng: 95,
-    math: 99,
-    physics: 84,
-    getAvg(){
-        let avg = (this.eng + this.math + this.physics) / 3;
-        console.log(avg);
-    }
-}
+// const student = {
+//     name: "Suraj",
+//     age: 20,
+//     eng: 95,
+//     math: 99,
+//     physics: 84,
+//     getAvg(){
+//         let avg = (this.eng + this.math + this.physics) / 3;
+//         console.log(avg);
+//     }
+// }
 
-console.log(student.getAvg());
+// console.log(student.getAvg());
 
 function getAvg(){
     console.log(this);
@@ -47,3 +47,40 @@ function getAvg(){
 setInterval( () => {
     console.log("Apna Desh");
 },2000);
+
+const student = {
+    name: "aman", //global scope 
+    marks: 95,
+    prop: this,
+    getName: function () {
+        return this.name;
+    },
+    getMarks: () => { //parent's scope -> window obj
+        console.log(this);
+        return this.marks; //undefined
+    },
+    getInfo1: function () {
+        setTimeout( () => {
+            console.log(this);// student obj
+        }, 2000);
+    },
+    getInfo2: function () {
+        setTimeout(function() {
+            console.log(this);// window object
+        }, 2000);
+    },
+};
+
+// console.log(student.getMarks());
+
+const square = (n) => n*n;
+console.log(square(4));
+
+// Write a function that prints "Hello world" 5 times at intervals of 2s each.
+let id = setInterval(() => {
+    console.log("Hello World");
+}, 2000);
+
+setTimeout(()=> {
+    clearInterval(id);
+},10000);
