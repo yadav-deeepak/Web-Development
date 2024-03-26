d64bedfa78fdb239715ffbfc6d40fce91399351f
Deepak Yadav <deepakyadav11767@gmail.com>
2024-03-26 18:41:34 +0530

    JS8

--- JavaScript/app8.js
+++ JavaScript/app8.js
@@ -24,3 +24,36 @@ let print = function (el){
 arr1.forEach((student) => {
     console.log(student.name);
 });
+let gpa =arr1.map((el) =>{
+     return el.marks/10;
+});
+console.log(gpa);
+
+let num =[1,2,3,4];
+      let double = num.map(function(el){
+        return el*2;
+      });
+
+let nums = [2,4,1,5,6,2,7,8,9];
+let even = nums.filter( (nums) =>(nums % 2 == 0) );
+console.log(nums);
+console.log(even);
+
+let val = [1,2,3,4].every( (el) => (el%2==0) ); //false
+let val1 = [2,4].every( (el) => (el%2==0) ); //true
+console.log(val,val1);
+
+let ans =[1,2,3,4].reduce( (res,el) => (res+el) ); 
+console.log(ans);
+
+//Using reduce method find the max in an array 
+let nums1 = [2,3,4,5,3,4,7,8,1,2];
+let result = nums1.reduce( (max,el) => {
+   if (el>max){
+      return el;
+   }
+   else{
+      return max;
+   }
+});
+console.log(result);
