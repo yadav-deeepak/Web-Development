038bcc566821342d265c606e195cba35a4462d8e
Deepak Yadav <deepakyadav11767@gmail.com>
2024-04-02 20:19:17 +0530

    JS9DOMF

--- JavaScript/app9dom.js
+++ JavaScript/app9dom.js
@@ -41,3 +41,44 @@ console.log(h4.parentElement);
 let ul = document.querySelector('ul');
 console.log(ul.children);
 console.log(ul.children[2].previousElementSibling);
+
+// let newP = document.createElement('p');
+// newP.innerText = "Hi this is new paragraph.";
+// newP.append("this is new text");
+ let body =document.querySelector('body');
+//  body.appendChild(newP);
+
+let btn = document.createElement('button');
+btn.innerHTML = "New button";
+let p = document.querySelector('p');
+console.log(btn);
+console.log(p);
+p.insertAdjacentElement('beforebegin', btn); //This will add the button before the paragraph starts.
+ p.insertAdjacentElement('afterbegin', btn); //This will add the element after the paragraph starts.At the start in the paragraph
+ p.insertAdjacentElement('beforeend', btn); //This will add the element before the paragraph end at the end in the paragraph
+ p.insertAdjacentElement('afterend', btn); //This will add the element after the paragraph ends.
+
+// p.remove();
+
+let redP =  document.createElement('p');
+redP.innerHTML = "Hey I'm red!!";
+redP.style.color = 'red';
+body.append(redP);
+
+let blueh3 = document.createElement('h3');
+blueh3.innerHTML= "I'm a blue H3";
+blueh3.style.color = 'blue';
+body.append(blueh3);
+
+let newDiv = document.createElement('div');
+newDiv.classList.add('divbox');
+body.append(newDiv);
+
+let h1 = document.createElement('h1');
+h1.innerHTML= "I'm in a div";
+newDiv.append(h1);
+
+let para1= document.createElement('p');
+para1.innerHTML = "ME TOO!";
+newDiv.append(para1);
+
