e457088091954cb9a053a5940bb434eadbca61ea
Deepak Yadav <deepakyadav11767@gmail.com>
2024-04-12 15:36:06 +0530

    JS12

---
+++ JavaScript/app12dom.js
@@ -0,0 +1,80 @@
+console.log("Working");
+
+async function greet(){
+    throw "Weak connection";
+    return "hello!";
+}
+
+greet()
+.then((result)=>{
+    console.log("Promise was resolved");
+    console.log("Result was: ",result);
+})
+.catch((err)=>{
+    console.log("Promise was rejected: ", err);
+});
+
+// Creating arrow function as async function 
+// let demo = async () => {
+//     return 5;
+// };
+
+// demo(); // Returns a promise with PromiseState as fulfilled.
+
+// Await 
+
+function getNum(){
+    return new Promise ((resolve,reject)=>{
+        setTimeout(()=>{
+            let num = Math.floor(Math.random()*10)+1;
+            console.log(num);
+            resolve();
+        },1000);
+    });
+}
+
+async function demo(){
+    await getNum();
+    await getNum();
+    await getNum();
+    await getNum();
+} // when we dont use await keyword then all the numbers will printed at a single time 
+// using await keyword it will wait till the first getNum() is resolved or rejected then the next getNum() will execute.
+
+let h1 = document.querySelector("h1");
+
+function changeColor(color,delay){
+   return new Promise((resolve,reject)=>{
+       setTimeout(()=>{
+            let num = Math.floor(Math.random()*5)+1;
+            if(num > 3){
+             reject("Promise rejected");
+            }
+           h1.style.color = color;
+           console.log(`color changed to ${color}!`);
+           resolve("Color Changed");
+       },delay);
+
+   });
+}
+
+async function demo(){
+
+    try{
+        await changeColor("red",1000);
+        await changeColor("orange",1000);
+        await changeColor("green",1000);
+        await changeColor("blue",1000);
+    }
+    catch(err){
+        console.log("Error caught");
+        console.log(err);
+    }
+    
+
+    let a=5;
+    console.log(a);
+    console.log("New number: ",a + 3);
+}
+// If promise gets rejected then the work that is not dependent on promise will also not be done 
+// demo();
