b80bf941ca3720d9ab3f6b0c8c2f7e0b75b524b3
Deepak Yadav <deepakyadav11767@gmail.com>
2024-04-16 17:22:27 +0530

    Nodejs Export and Require

--- Nodejs/script.js
+++ Nodejs/script.js
@@ -1,3 +1,17 @@
+const math = require("./math"); //by using require keyword we can require and use the value that we have exported in math.js in script.js
+
+console.log(math.sum(2,2)); // We will get the value that we have exported in our math.js file as output.
+console.log(math.mul(2,4));
+console.log(math.PI);
+
+
+
+
+
+
+
+
+
 // let n =5;
 
 // for(let i=0;i<n;i++){
@@ -6,7 +20,7 @@
 
 
 
-let args = process.argv; // Using this we will get all the arguments that are passed as argument while using node script.js Deepak Viper Suraj here 'Deepak Viper Suraj' these are arguments 
-for(let i=2;i< args.length;i++){
-   console.log("Hello and welcome",args[i]);
-}
+// let args = process.argv; // Using this we will get all the arguments that are passed as argument while using node script.js Deepak Viper Suraj here 'Deepak Viper Suraj' these are arguments 
+// for(let i=2;i< args.length;i++){
+//    console.log("Hello and welcome",args[i]);
+// }
