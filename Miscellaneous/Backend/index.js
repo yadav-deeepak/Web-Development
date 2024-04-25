1d897508b1229a2f5b8985b6b8b939f1f02d5d9d
Deepak Yadav <deepakyadav11767@gmail.com>
2024-04-25 19:45:20 +0530

    Revisiting JS

--- Miscellaneous/Backend/index.js
+++ Miscellaneous/Backend/index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 let port = 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.get("/register",(req,res)=>{
     let {user , password} = req.query;
     res.send(`Standard get response .Welcome ${user}`);
 });
 
 app.post("/register",(req,res)=>{
-    res.send("Standard Post response");
+    let {user , password} = req.body;
+    res.send(`Standard get response .Welcome ${user}`);
 });
 
 app.listen(port,() =>{
---
+++ Miscellaneous/Frontend/app.js
@@ -0,0 +1,14 @@
+const stu1 = {
+    name: "suraj",
+    age: 20, 
+    marks: 99,
+};
+
+const stu2 = {
+    name: "Deepak",
+    age: 22, 
+    marks: 82,
+};
+
+let arr =[1,2,3];
+arr.sayHello = () => {console.log("Hello I'm arr");};
