39f6728ea743a292e3c91866490efede770aa7da
Deepak Yadav <deepakyadav11767@gmail.com>
2024-04-26 22:17:54 +0530

    Revisiting JS

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -12,3 +12,30 @@ const stu2 = {
 
 let arr =[1,2,3];
 arr.sayHello = () => {console.log("Hello I'm arr");};
+
+// FACTORY FUNCTION //
+
+// function personMaker(name,age){
+//     const person={
+//         name: name,
+//         age: age,
+//         talk() {
+//             console.log(`Hi my name is ${this.name}`);
+//         }
+//     };
+//     return person;
+// }
+
+//  ANOTHER WAY USING CONSTRUCTOR  //
+
+function Person(name,age){
+    this.name = name;
+    this.age = age;
+}
+
+Person.prototype.talk = function () {
+    console.log(`Hi , my name is ${this.name}`);
+};
+
+let p1 = new Person("adam",25);
+let p2 = new Person("eve",24);
