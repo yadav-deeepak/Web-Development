ae4c1cb8dc201f3c7b5383f731a489cbb1908149
Deepak Yadav <deepakyadav11767@gmail.com>
2024-04-30 16:42:25 +0530

    Revisiting JS OOPs

--- Miscellaneous/Frontend/app.js
+++ Miscellaneous/Frontend/app.js
@@ -28,14 +28,40 @@ arr.sayHello = () => {console.log("Hello I'm arr");};
 
 //  ANOTHER WAY USING CONSTRUCTOR  //
 
-function Person(name,age){
-    this.name = name;
-    this.age = age;
+// function Person(name,age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// Person.prototype.talk = function () {
+//     console.log(`Hi , my name is ${this.name}`);
+// };
+
+
+//using class 
+
+class Person {
+    constructor (name , age){
+        this.name = name;
+        this.age = age;
+    }
+    talk(){
+        console.log(`Hi, my name is ${this.name}`);
+    }
 }
 
-Person.prototype.talk = function () {
-    console.log(`Hi , my name is ${this.name}`);
-};
+class Student extends Person {
+    constructor (name,age,marks){
+        super(name,age); // When we use super constructor then that means that we are calling the parent class constructor.
+        this.marks = marks;
+    }
+    greet(){
+        return "Hello!";
+    }
+ }
+
+ let s1 = new Student("adam",25,95);
+
 
 let p1 = new Person("adam",25);
 let p2 = new Person("eve",24);
