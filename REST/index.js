e1fcbdc5f91fc1dfdee05adf5e7dc6e2efa84961
Deepak Yadav <deepakyadav11767@gmail.com>
2024-05-02 19:38:02 +0530

    Creating Quora like website

--- REST/index.js
+++ REST/index.js
@@ -2,16 +2,64 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
-app.set(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"public")));
 
-app.get("/",(req,res) =>{
-    res.send("Server working well!!");
+
+// POST DATA  //
+let posts = [
+    {
+        id : uuidv4(),
+        username : "apnacollege",
+        content : "I love coding!"
+    },
+    {
+        id : uuidv4(),
+        username : "deepakyadav",
+        content : "discipline is the key to success"
+    },
+    {
+        id : uuidv4(),
+        username : "harsh",
+        content : "got selected for my first internship"
+    },
+];
+
+app.get("/posts",(req,res) =>{
+    res.render("index.ejs",{ posts });
+});
+
+app.get("/posts/new",(req,res)=>{
+    res.render("new.ejs");
+});
+
+app.post("/posts",(req,res)=>{
+    
+    let {username, content} = req.body;
+    let id = uuidv4();
+    posts.push({id ,username, content});
+    res.redirect("/posts");
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id ===p.id); // Finding the post
+    post.content = newContent;
+    console.log(post);
+    res.send("Patch request working");
+});
+
+app.get("/posts/:id",(req,res) =>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id ===p.id);
+    res.render("show.ejs",{post});
 });
 
 app.listen(port,() =>{
