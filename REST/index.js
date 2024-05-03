b8dc79b55caf4aa3da9ec69e728af4c6d7c8713d
Deepak Yadav <deepakyadav11767@gmail.com>
2024-05-03 16:32:53 +0530

    Created Quora website

--- REST/index.js
+++ REST/index.js
@@ -3,8 +3,10 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const { v4: uuidv4 } = require('uuid');
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
@@ -53,13 +55,25 @@ app.patch("/posts/:id",(req,res)=>{
     let post = posts.find((p)=> id ===p.id); // Finding the post
     post.content = newContent;
     console.log(post);
-    res.send("Patch request working");
+    res.redirect("/posts");
 });
+
+app.get("/posts/:id/edit",(req,res)=> {
+    let {id} = req.params;
+    let post = posts.find((p)=> id ===p.id);
+    res.render("edit.ejs",{post});
+})
 
 app.get("/posts/:id",(req,res) =>{
     let {id} = req.params;
     let post = posts.find((p)=> id ===p.id);
     res.render("show.ejs",{post});
+});
+
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter((p)=> id != p.id);
+    res.redirect("/posts");
 });
 
 app.listen(port,() =>{
