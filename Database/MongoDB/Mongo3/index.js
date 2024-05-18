3468e918dc7235a424ae1e2aef842fedf23162b4
Deepak Yadav <deepakyadav11767@gmail.com>
2024-05-18 21:31:11 +0530

    MongoDB & Express

--- Database/MongoDB/Mongo3/index.js
+++ Database/MongoDB/Mongo3/index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chats.js");
 const port = 3000;
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"public")));
 
 main().then((res)=>{
     console.log("Connection successful");
@@ -15,6 +18,13 @@ main().then((res)=>{
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
 }
+
+//Index Route 
+app.get("/chats",async (req,res)=>{
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+});
 
 app.get("/",(req,res)=>{
     res.send("Home page");
---
+++ Database/MongoDB/Mongo3/init.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chats.js");
+
+main().then((res)=>{
+    console.log("Connection successful");
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+let allChats = [
+    {
+        from: "Suraj",
+        to: "Deepak",
+        msg: "Oye order lelo",
+        created_at: new Date(),
+    },
+    {
+        from: "Viper",
+        to: "Deepak",
+        msg: "Photos bhejde",
+        created_at: new Date(),
+    },
+    {
+        from: "Suraj",
+        to: "Viper",
+        msg: "Chemistry kaha se padhu",
+        created_at: new Date(),
+    },
+    {
+        from: "Viper",
+        to: "Harsh",
+        msg: "Glitchzz",
+        created_at: new Date(),
+    },
+]
+
+Chat.insertMany(allChats);
+
