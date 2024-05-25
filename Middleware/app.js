0a29aff26fcdf900d66689d880e1b2e1a3082567
Deepak Yadav <deepakyadav11767@gmail.com>
2024-05-25 22:13:48 +0530

    Middlewares

--- Middleware/app.js
+++ Middleware/app.js
@@ -4,8 +4,29 @@ const port = 3000;
 
 //middleware -> response send
 
+// app.use((req,res,next)=>{
+//     console.log("Hi , I'm 1st middleware");
+//     next();
+// });
+
+// app.use((req,res,next)=>{
+//     console.log("Hi , I'm 2nd middleware");
+//     next();
+// });
+
+//Logger 
+app.use((req,res,next)=>{
+    req.time = new Date(Date.now()).toString();
+    console.log(req.method,req.hostname,req.path,req.time);
+    next();
+});
+
 app.get("/",(req,res)=>{
-    res.send();
+    res.send("Hi I'm root");
+});
+
+app.get("/random",(req,res)=>{
+    res.send("This is a random page");
 });
 
 app.listen(port,()=>{
