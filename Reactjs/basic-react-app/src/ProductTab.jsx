ec85288bda4be3d12c5c9272419c26514cf76d0d
Deepak Yadav <deepakyadav11767@gmail.com>
2024-06-03 23:01:59 +0530

    React Props

--- Reactjs/basic-react-app/src/ProductTab.jsx
+++ Reactjs/basic-react-app/src/ProductTab.jsx
@@ -3,9 +3,9 @@ import Product from "./Product";
 function ProductTab() {
     return(
         <>
-        <Product></Product>
-        <Product></Product>
-        <Product></Product>
+        <Product title="iPhone" price="50k"></Product>
+        <Product title="Laptop" price="0k"></Product>
+        <Product title="Pen" price="10"></Product>
         </>
     );
 }
