0202df3aa94d42e109fe6d350a2547d6e9734d73
Deepak Yadav <deepakyadav11767@gmail.com>
2024-06-05 22:54:30 +0530

    React

--- Reactjs/basic-react-app/src/Product.jsx
+++ Reactjs/basic-react-app/src/Product.jsx
@@ -2,12 +2,12 @@ import "./Product.css";
 
 function Product({title, price, features, features2}) {
     console.log(features);
+    // const list = features.map((feature) => <li>{feature}</li>);
     return(
         <div className="Product">
             <h3>{title}</h3>
             <h5>Price: {price}</h5>
-            <p>{features}</p>
-            <p>{features2.b}</p>
+            {price > 30000 ? <p>"Discount of 5%"</p> : null}
         </div>
     );
 }
--- Reactjs/basic-react-app/src/ProductTab.jsx
+++ Reactjs/basic-react-app/src/ProductTab.jsx
@@ -1,9 +1,12 @@
 import Product from "./Product";
 
 function ProductTab() { 
+    let options =["hi-tech", "durable", "fast"];
     return(
         <>
-        <Product title="iPhone" price="50k" features={["hi-tech", "durable", "fast"]} features2={{ a: "hi-tech", b: "durable", c: "fast"}}></Product>
+        <Product title="iPhone" price="50000"></Product>
+        <Product title="RealMe" price={30000}></Product>
+        <Product title="Oppo A9" price="15000"></Product>
         </>
     );
 }
