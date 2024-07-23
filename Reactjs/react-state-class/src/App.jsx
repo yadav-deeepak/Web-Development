497390111bb85328ffc1b784cf056fed940b52ed
Deepak Yadav <deepakyadav11767@gmail.com>
2024-07-23 22:15:26 +0530

    Lottery game

--- Reactjs/react-state-class/src/App.jsx
+++ Reactjs/react-state-class/src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import LudoBoard from './LudoBoard';
 import TodoList from './TodoList';
 import Lottery from './Lottery';
+import Ticket from './Ticket';
 
 function App() {
     return (
     <>
-    <Lottery></Lottery>
+    <Ticket ticket={[0,1,2]}></Ticket>
+    <Ticket ticket={[4,5,6,7,8]}></Ticket>
     </>
   );
 }
