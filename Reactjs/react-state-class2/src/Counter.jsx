5499b112178bcefdd46f8bb26b1525e9bff9e285
Deepak Yadav <deepakyadav11767@gmail.com>
2024-09-09 22:28:51 +0530

    React useEffect

--- Reactjs/react-state-class2/src/Counter.jsx
+++ Reactjs/react-state-class2/src/Counter.jsx
@@ -1,20 +1,27 @@
 import { useState,useEffect } from "react";
 
 export default function Counter(){
-    let [count, setCount] = useState(0);
+    let [countx, setCountx] = useState(0);
+    let [county, setCounty] = useState(0);
 
-    let incCount = () =>{
-        setCount((currCount) => currCount +1);
+    let incCountx = () =>{
+        setCountx((currCount) => currCount +1);
+    };
+
+    let incCounty = () =>{
+        setCounty((currCount) => currCount +1);
     };
 
     useEffect(function sideEffect(){
         console.log("This is the side effect");
-    });
+    },[countx]);
 
     return (
         <div>
-            <h3>Count= {count} </h3>
-            <button onClick={incCount}>+1</button>
+            <h3>Count= {countx} </h3>
+            <button onClick={incCountx}>+1</button>
+            <h3>Count= {county} </h3>
+            <button onClick={incCounty}>+1</button>
         </div>
     );
 }
