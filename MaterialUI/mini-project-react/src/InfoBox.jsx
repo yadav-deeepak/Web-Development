773606e6c2ea909e57e58a04be6fe98927eb5cd1
Deepak Yadav <deepakyadav11767@gmail.com>
2024-09-20 21:33:36 +0530

    Material UI

--- MaterialUI/mini-project-react/src/InfoBox.jsx
+++ MaterialUI/mini-project-react/src/InfoBox.jsx
@@ -2,10 +2,16 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import ThunderstormIcon from '@mui/icons-material/Thunderstorm';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
+import WbSunnyIcon from '@mui/icons-material/WbSunny';
 import "./InfoBox.css";
 
  export default function InfoBox({info}){
     const INIT_URL = "https://images.unsplash.com/photo-1579003593419-98f949b9398f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+    const HOT_URL = "https://images.unsplash.com/photo-1581129724980-2ab2153c3d8d?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const COLD_URL = "https://images.unsplash.com/photo-1564314966899-839134794c8c?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+    const RAIN_URL = "https://images.unsplash.com/photo-1470432581262-e7880e8fe79a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHJhaW55JTIwd2VhdGpoZXJ8ZW58MHx8MHx8fDA%3D";
 
     return (
         <div className="InfoBox">
@@ -13,12 +19,12 @@ import "./InfoBox.css";
        <Card sx={{ maxWidth: 345 }}>
       <CardMedia
         sx={{ height: 140 }}
-        image={INIT_URL}
+        image={info.humidity >80 ? RAIN_URL : info.temp > 15 ? HOT_URL : COLD_URL}
         title="green iguana"
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {info.city}
+          {info.city} {info.humidity >80 ? <ThunderstormIcon></ThunderstormIcon> : info.temp > 15 ? <WbSunnyIcon></WbSunnyIcon> : <AcUnitIcon></AcUnitIcon>}
         </Typography>
         <Typography variant="body2" sx={{ color: 'text.secondary' }} component={"span"}>
           <p>Temperature = {info.temp}&deg;C</p>
